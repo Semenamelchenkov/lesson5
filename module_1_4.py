8b0a9b68df767ad52829dce7d37a474893f15a46
Semenamelchenkov <amelchenkov-semen@yandex.ru>
2024-09-01 11:56:11 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,15 @@
+my_string=input(f'Введите ваше имя: ')
+my_string=f'Введите ваше имя: '
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
+
+
+
+
+
+
+
